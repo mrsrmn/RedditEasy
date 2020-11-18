201cae96897cbd91f1a1a0b4696fb12577cc42ce
emirsurmen <63517174+emirsurmen@users.noreply.github.com>
2020-11-18 18:56:13 +0300

    Added new functions

--- redditeasy/random.py
+++ redditeasy/random.py
@@ -26,7 +26,7 @@ class Random:
 
     def get_post(self):
         """
-        :return: (str) Info about the randomly selected post
+        :return: (str) Info about the randomly selected post from the subreddit (hot)
         """
 
         randompost = random.randint(1, 25)
@@ -58,11 +58,75 @@ class Random:
 
     def get_top_post(self):
         """
-        :return: (str) Info about the randomly selected post
+        :return: (str) Info about the randomly selected post (top)
         """
 
         randompost = random.randint(1, 25)
         request = requests.get(f"https://www.reddit.com/r/{self.subreddit}/top.json")
+        meme = json.loads(request.content)
+        nsfw = meme["data"]["children"][randompost]["data"]["over_18"]
+        pinned = meme["data"]["children"][randompost]["data"]["pinned"]
+
+        if nsfw == "true":
+            nsfw = True
+        elif nsfw == "false":
+            nsfw = False
+
+        if pinned == "true":
+            pinned = True
+        elif pinned == "false":
+            pinned = False
+
+        return Reddit(
+            image_link=meme["data"]["children"][randompost]["data"]["url_overridden_by_dest"],
+            title=meme["data"]["children"][randompost]["data"]["title"],
+            upvote_ratio=meme["data"]["children"][randompost]["data"]["upvote_ratio"],
+            total_awards=meme["data"]["children"][randompost]["data"]["total_awards_received"],
+            score=meme["data"]["children"][randompost]["data"]["score"],
+            downvotes=meme["data"]["children"][randompost]["data"]["downs"],
+            nsfw=nsfw,
+            pinned=pinned
+        )
+
+    def get_new_post(self):
+        """
+        :return: (str) Info about the randomly selected post (new)
+        """
+
+        randompost = random.randint(1, 25)
+        request = requests.get(f"https://www.reddit.com/r/{self.subreddit}/new.json")
+        meme = json.loads(request.content)
+        nsfw = meme["data"]["children"][randompost]["data"]["over_18"]
+        pinned = meme["data"]["children"][randompost]["data"]["pinned"]
+
+        if nsfw == "true":
+            nsfw = True
+        elif nsfw == "false":
+            nsfw = False
+
+        if pinned == "true":
+            pinned = True
+        elif pinned == "false":
+            pinned = False
+
+        return Reddit(
+            image_link=meme["data"]["children"][randompost]["data"]["url_overridden_by_dest"],
+            title=meme["data"]["children"][randompost]["data"]["title"],
+            upvote_ratio=meme["data"]["children"][randompost]["data"]["upvote_ratio"],
+            total_awards=meme["data"]["children"][randompost]["data"]["total_awards_received"],
+            score=meme["data"]["children"][randompost]["data"]["score"],
+            downvotes=meme["data"]["children"][randompost]["data"]["downs"],
+            nsfw=nsfw,
+            pinned=pinned
+        )
+
+    def get_controversial_post(self):
+        """
+        :return: (str) Info about the randomly selected post (new)
+        """
+
+        randompost = random.randint(1, 25)
+        request = requests.get(f"https://www.reddit.com/r/{self.subreddit}/controversial.json")
         meme = json.loads(request.content)
         nsfw = meme["data"]["children"][randompost]["data"]["over_18"]
         pinned = meme["data"]["children"][randompost]["data"]["pinned"]
--- redditeasy/user.py
+++ redditeasy/user.py
@@ -1,10 +1,74 @@
 import requests
 import json
 import random
-import datetime
+from .reddit import Reddit
 
 
 class User:
     def __init__(self, user):
         self.user = user
 
+    def get_post(self):
+        """
+        :return: (str) Info about the randomly selected post from the user
+        """
+
+        randompost = random.randint(1, 25)
+        request = requests.get(f"https://www.reddit.com/u/{self.user}/hot.json")
+        meme = json.loads(request.content)
+        nsfw = meme["data"]["children"][randompost]["data"]["over_18"]
+        pinned = meme["data"]["children"][randompost]["data"]["pinned"]
+
+        if nsfw == "true":
+            nsfw = True
+        elif nsfw == "false":
+            nsfw = False
+
+        if pinned == "true":
+            pinned = True
+        elif pinned == "false":
+            pinned = False
+
+        return Reddit(
+            image_link=meme["data"]["children"][randompost]["data"]["url_overridden_by_dest"],
+            title=meme["data"]["children"][randompost]["data"]["title"],
+            upvote_ratio=meme["data"]["children"][randompost]["data"]["upvote_ratio"],
+            total_awards=meme["data"]["children"][randompost]["data"]["total_awards_received"],
+            score=meme["data"]["children"][randompost]["data"]["score"],
+            downvotes=meme["data"]["children"][randompost]["data"]["downs"],
+            nsfw=nsfw,
+            pinned=pinned
+        )
+
+    def get_top_post(self):
+        def get_post(self):
+            """
+            :return: (str) Info about the randomly selected top post from the user
+            """
+
+            randompost = random.randint(1, 25)
+            request = requests.get(f"https://www.reddit.com/u/{self.user}/top.json")
+            meme = json.loads(request.content)
+            nsfw = meme["data"]["children"][randompost]["data"]["over_18"]
+            pinned = meme["data"]["children"][randompost]["data"]["pinned"]
+
+            if nsfw == "true":
+                nsfw = True
+            elif nsfw == "false":
+                nsfw = False
+
+            if pinned == "true":
+                pinned = True
+            elif pinned == "false":
+                pinned = False
+
+            return Reddit(
+                image_link=meme["data"]["children"][randompost]["data"]["url_overridden_by_dest"],
+                title=meme["data"]["children"][randompost]["data"]["title"],
+                upvote_ratio=meme["data"]["children"][randompost]["data"]["upvote_ratio"],
+                total_awards=meme["data"]["children"][randompost]["data"]["total_awards_received"],
+                score=meme["data"]["children"][randompost]["data"]["score"],
+                downvotes=meme["data"]["children"][randompost]["data"]["downs"],
+                nsfw=nsfw,
+                pinned=pinned
+            )
