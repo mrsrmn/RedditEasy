973f7e89795951fa90ad195f1eca101224427f9a
emirsurmen <63517174+emirsurmen@users.noreply.github.com>
2020-11-18 21:26:09 +0300

    Added examples

---
+++ examples/memes.py
@@ -0,0 +1,10 @@
+import redditeasy
+
+post = redditeasy.Subreddit("memes")
+postoutput = post.get_post()
+
+print(f"Posts Title: {postoutput.title}\n"
+      f"Posts Image Link: {postoutput.image_link}\n"
+      f"Post Created At: {postoutput.created_at}\n"
+      f"Posts Upvote Count: {postoutput.score}\n"
+      f"Posts Award Count: {postoutput.total_awards}")
