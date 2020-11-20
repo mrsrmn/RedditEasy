75ed3c3fbb2fd8f79cce5230c754878791c3b486
emirsurmen <63517174+emirsurmen@users.noreply.github.com>
2020-11-20 14:31:29 +0300

    Changed the version

--- redditeasy/__init__.py
+++ redditeasy/__init__.py
@@ -2,4 +2,4 @@ from .subreddit import Subreddit
 from .reddit import Reddit
 from .user import User
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="redditeasy",
-    version="0.1.5",
+    version="0.1.6",
     author="MakufonSkifto",
     author_email="emirsurmen@gmail.com",
     license="MIT",
