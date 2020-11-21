05308a55ea28fc65e5fc80e06f360b132eb665c6
emirsurmen <63517174+emirsurmen@users.noreply.github.com>
2020-11-21 22:09:38 +0300

    Updated the version and uploaded to PyPI

--- redditeasy/__init__.py
+++ redditeasy/__init__.py
@@ -2,4 +2,4 @@ from .subreddit import Subreddit
 from .reddit import Reddit
 from .user import User
 
-__version__ = "0.2.0"
+__version__ = "1.0.0"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="redditeasy",
-    version="0.2.0",
+    version="1.0.0",
     author="MakufonSkifto",
     author_email="emirsurmen@gmail.com",
     license="MIT",
