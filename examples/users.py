ce748603e52edd40016790db330f48800c2bc1dc
emirsurmen <63517174+emirsurmen@users.noreply.github.com>
2020-11-29 13:23:58 +0300

    Changed the example

--- examples/users.py
+++ examples/users.py
@@ -12,4 +12,5 @@ print(f"Posts Title: {postoutput.title}\n"
       f"Posts Upvote Count: {postoutput.score}\n"
       f"Posts Award Count: {postoutput.total_awards}\n"
       f"NSFW?: {postoutput.nsfw}\n"
-      f"fart: {postoutput.post_flair}")
+      f"Post Flair: {postoutput.post_flair}\n"
+      f"User Flair: {postoutput.author_flair}")
