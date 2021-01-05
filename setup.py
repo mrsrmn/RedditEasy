47fdbabbcb594f1cc133071eae263cdeb0ccc2c0
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2021-01-05 20:26:02 +0300

    Added python-dotenv to requirements.txt

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/MakufonSkifto/RedditEasy",
     packages=setuptools.find_packages(),
-    install_requires=["requests", "ujson"],
+    install_requires=["requests", "ujson", "python-dotenv"],
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
