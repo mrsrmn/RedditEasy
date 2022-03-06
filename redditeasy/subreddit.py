7832b3f694bd700bbe3766f1438bb4603a009f97
emirsurmen <emirsurmen@gmail.com>
2022-03-06 15:08:06 +0300

    Add missing parameters to docstrings and param types

--- redditeasy/subreddit.py
+++ redditeasy/subreddit.py
@@ -7,34 +7,37 @@ class Subreddit(SubredditBase):
     This class is for getting posts from a subreddit
     """
 
-    def get_post(self, subreddit) -> Post:
+    def get_post(self, subreddit: str) -> Post:
         """
         :return: The randomly selected post from the subreddit (hot)
+        :param: subreddit (:obj:`builtins.str`) - The subreddit to get the post from
         :rtype: Post
         """
 
         return get_post(self, rtype="hot", rfor=subreddit, slash="r")
 
-    def get_top_post(self, subreddit) -> Post:
+    def get_top_post(self, subreddit: str) -> Post:
         """
-        :return: The randomly selected post from the subreddit (This will return the TOP POST OF TODAY,
-         not the top post of all time)
+        :return: The randomly selected post from the subreddit (top)
+        :param: subreddit (:obj:`builtins.str`) - The subreddit to get the post from
         :rtype: Post
         """
 
         return get_post(self, rtype="top", rfor=subreddit, slash="r")
 
-    def get_new_post(self, subreddit) -> Post:
+    def get_new_post(self, subreddit: str) -> Post:
         """
         :return: The randomly selected post from the subreddit (new)
+        :param: subreddit (:obj:`builtins.str`) - The subreddit to get the post from
         :rtype: Post
         """
 
         return get_post(self, rtype="new", rfor=subreddit, slash="r")
 
-    def get_controversial_post(self, subreddit) -> Post:
+    def get_controversial_post(self, subreddit: str) -> Post:
         """
         :return: The randomly selected post from the subreddit (controversial)
+        :param: subreddit (:obj:`builtins.str`) - The subreddit to get the post from
         :rtype: Post
         """
 
@@ -46,34 +49,37 @@ class AsyncSubreddit(SubredditBase):
     This class is for getting posts from a subreddit in an async way
     """
 
-    async def get_post(self, subreddit) -> Post:
+    async def get_post(self, subreddit: str) -> Post:
         """
         :return: The randomly selected post from the subreddit (hot)
+        :param: subreddit (:obj:`builtins.str`) - The subreddit to get the post from
         :rtype: Post
         """
 
         return await get_async_post(self, rtype="hot", rfor=subreddit, slash="r")
 
-    async def get_top_post(self, subreddit) -> Post:
+    async def get_top_post(self, subreddit: str) -> Post:
         """
-        :return: The randomly selected post from the subreddit (This will return the TOP POST OF TODAY,
-         not the top post of all time)
+        :return: The randomly selected post from the subreddit (top)
+        :param: subreddit (:obj:`builtins.str`) - The subreddit to get the post from
         :rtype: Post
         """
 
         return await get_async_post(self, rtype="top", rfor=subreddit, slash="r")
 
-    async def get_new_post(self, subreddit) -> Post:
+    async def get_new_post(self, subreddit: str) -> Post:
         """
         :return: Info about the randomly selected post from the subreddit (new)
+        :param: subreddit (:obj:`builtins.str`) - The subreddit to get the post from
         :rtype: Post
         """
 
         return await get_async_post(self, rtype="new", rfor=subreddit, slash="r")
 
-    async def get_controversial_post(self, subreddit) -> Post:
+    async def get_controversial_post(self, subreddit: str) -> Post:
         """
         :return: Info about the randomly selected post from the subreddit (controversial)
+        :param: subreddit (:obj:`builtins.str`) - The subreddit to get the post from
         :rtype: Post
         """
 
--- redditeasy/user.py
+++ redditeasy/user.py
@@ -7,31 +7,38 @@ class User(UserBase):
     This class is for getting posts from a user
     """
 
-    def get_post(self, user) -> Post:
+    def get_post(self, user: str) -> Post:
         """
-        :return: Info about the randomly selected post from the user
+        :return: Info about the randomly selected post from the user (hot)
+        :param: user (:obj:`builtins.str`) - The user to get the post from
+        :rtype: Post
         """
 
         return get_post(self, rtype="hot", rfor=user, slash="u")
 
-    def get_top_post(self, user) -> Post:
+    def get_top_post(self, user: str) -> Post:
         """
-        :return: Info about the randomly selected post from the user (This will return the TOP POST OF TODAY,
-         not the top post of all time)
+        :return: Info about the randomly selected post from the user (top)
+        :param: user (:obj:`builtins.str`) - The user to get the post from
+        :rtype: Post
         """
 
         return get_post(self, rtype="top", rfor=user, slash="u")
 
-    def get_new_post(self, user) -> Post:
+    def get_new_post(self, user: str) -> Post:
         """
         :return: Info about the randomly selected post from the user (new)
+        :param: user (:obj:`builtins.str`) - The user to get the post from
+        :rtype: Post
         """
 
         return get_post(self, rtype="new", rfor=user, slash="u")
 
-    def get_controversial_post(self, user) -> Post:
+    def get_controversial_post(self, user: str) -> Post:
         """
-        :return: Info about the randomly selected post from the user (new)
+        :return: Info about the randomly selected post from the user (controversial)
+        :param: user (:obj:`builtins.str`) - The user to get the post from
+        :rtype: Post
         """
 
         return get_post(self, rtype="controversial", rfor=user, slash="u")
@@ -42,31 +49,38 @@ class AsyncUser(UserBase):
     This class is for getting posts from a user in an async way
     """
 
-    async def get_post(self, user) -> Post:
+    async def get_post(self, user: str) -> Post:
         """
-        :return: Info about the randomly selected post from the user
+        :return: Info about the randomly selected post from the user (hot)
+        :param: user (:obj:`builtins.str`) - The user to get the post from
+        :rtype: Post
         """
 
         return await get_async_post(self, rtype="hot", rfor=user, slash="u")
 
-    async def get_top_post(self, user) -> Post:
+    async def get_top_post(self, user: str) -> Post:
         """
-        :return: Info about the randomly selected post from the user (This will return the TOP POST OF TODAY,
-         not the top post of all time)
+        :return: Info about the randomly selected post from the user (top)
+        :param: user (:obj:`builtins.str`) - The user to get the post from
+        :rtype: Post
         """
 
         return await get_async_post(self, rtype="top", rfor=user, slash="u")
 
-    async def get_new_post(self, user) -> Post:
+    async def get_new_post(self, user: str) -> Post:
         """
         :return: Info about the randomly selected post from the user (new)
+        :param: user (:obj:`builtins.str`) - The user to get the post from
+        :rtype: Post
         """
 
         return await get_async_post(self, rtype="new", rfor=user, slash="u")
 
-    async def get_controversial_post(self, user) -> Post:
+    async def get_controversial_post(self, user: str) -> Post:
         """
-        :return: Info about the randomly selected post from the user (new)
+        :return: Info about the randomly selected post from the user (controversial)
+        :param: user (:obj:`builtins.str`) - The user to get the post from
+        :rtype: Post
         """
 
         return await get_async_post(self, rtype="controversial", rfor=user, slash="u")
